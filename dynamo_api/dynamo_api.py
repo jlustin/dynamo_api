3ab33f92dc1295cc3093f98ac0a274a1472efdc6
Justin Lei <jun.lei@mail.mcgill.ca>
2022-03-31 22:29:22 -0400

    add get, update, delete operations (#3)

--- dynamo_api/dynamo_api.py
+++ dynamo_api/dynamo_api.py
@@ -1,4 +1,5 @@
 import boto3
+from botocore.exceptions import ClientError
 
 
 def create_resource(config):
@@ -16,3 +17,38 @@ def put(table_name, item, dynamodb):
     table = dynamodb.Table(table_name)
     response = table.put_item(Item=item)
     return response
+
+
+def get(table_name, key, dynamodb):
+    table = dynamodb.Table(table_name)
+    try:
+        response = table.get_item(Key=key)
+    except ClientError as e:
+        msg = e.response['Error']['Message']
+        print(msg)
+        raise
+    else:
+        return response['Item']
+
+
+def update(table_name, key, update_expr, expr_values, dynamodb):
+    table = dynamodb.Table(table_name)
+    response = table.update_item(
+        Key=key,
+        UpdateExpression=update_expr,
+        ExpressionAttributeValues=expr_values,
+        ReturnValues='UPDATED_NEW'
+    )
+    return response['Attributes']
+
+
+def delete(table_name, key, dynamodb):
+    table = dynamodb.Table(table_name)
+    try:
+        response = table.delete_item(Key=key)
+    except ClientError as e:
+        msg = e.response['Error']['Message']
+        print(msg)
+        raise
+    else:
+        return response
--- tests/test_dynamo_api.py
+++ tests/test_dynamo_api.py
@@ -1,5 +1,12 @@
+import pytest
+
+from botocore.exceptions import ClientError
 from dynamo_api import __version__
-from dynamo_api.dynamo_api import create_resource, put
+from dynamo_api.dynamo_api import create_resource, put, get, update, delete
+
+
+def test_version():
+    assert __version__ == '0.1.4'
 
 
 config = {
@@ -36,10 +43,6 @@ def setup_function():
     )
 
 
-def test_version():
-    assert __version__ == '0.1.3'
-
-
 def test_create_resource():
     conn = create_resource(config)
     assert conn is not None
@@ -52,6 +55,60 @@ def test_put():
     }
     response = put("Test", test_object, conn)
     assert response['ResponseMetadata']['HTTPStatusCode'] == 200
+
+
+def test_get():
+    conn = create_resource(config)
+    test_object = {
+        'id': 1,
+        'name': 'test'
+    }
+    put("Test", test_object, conn)
+    response = get("Test", {'id': 1}, conn)
+    assert response == test_object
+
+
+def test_get_error():
+    conn = create_resource(config)
+    test_object = {
+        'id': 1,
+        'name': 'test'
+    }
+    put("Test", test_object, conn)
+    with pytest.raises(ClientError):
+        get("Test", {'name': 'test1'}, conn)
+
+
+def test_update():
+    conn = create_resource(config)
+    test_object = {
+        'id': 1,
+        'test_name': 'test'
+    }
+    put("Test", test_object, conn)
+    update_expr = "set test_name=:n"
+    expr_values = {
+        ':n': 'passed'
+    }
+    response = update("Test", {'id': 1}, update_expr, expr_values, conn)
+    assert response['test_name'] == 'passed'
+
+
+def test_delete():
+    conn = create_resource(config)
+    test_object = {
+        'id': 1,
+        'test_name': 'test'
+    }
+    put("Test", test_object, conn)
+    response = delete("Test", {'id': 1}, conn)
+    assert response['ResponseMetadata']['HTTPStatusCode'] == 200
+
+
+def test_delete_error():
+    conn = create_resource(config)
+    with pytest.raises(ClientError):
+        delete("Test", {'name': 1}, conn)
 
 
 def teardown_function():
